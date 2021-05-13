c8c9836cfd45d38bc54a54f6c31e3d03bced1dd2
Pascal Honoré <pascal@alseyn.net>
2021-05-13 23:34:48 +0100

    Introduce Arrows

---
+++ Libs/Arrows.rb
@@ -0,0 +1,71 @@
+
+# encoding: UTF-8
+
+class Arrows
+
+    # Arrows::databaseFilepath()
+    def self.databaseFilepath()
+        "#{Config::nyxFolderPath()}/arrows.sqlite3"
+    end
+
+    # Arrows::insert(sourceuuid, targetuuid)
+    def self.insert(sourceuuid, targetuuid)
+        db = SQLite3::Database.new(Arrows::databaseFilepath())
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.execute "delete from _arrows_ where _sourceuuid_=? and _targetuuid_=?", [sourceuuid, targetuuid]
+        db.execute "insert into _arrows_ (_sourceuuid_, _targetuuid_) values (?, ?)", [sourceuuid, targetuuid]
+        db.close
+    end
+
+    # Arrows::delete(sourceuuid, targetuuid)
+    def self.delete(sourceuuid, targetuuid)
+        db = SQLite3::Database.new(Arrows::databaseFilepath())
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.execute "delete from _arrows_ where _sourceuuid_=? and _targetuuid_=?", [sourceuuid, targetuuid]
+        db.close
+    end
+
+    # Arrows::targetsuuids(sourceuuid)
+    def self.targetsuuids(sourceuuid)
+        db = SQLite3::Database.new(Arrows::databaseFilepath())
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        answer = []
+        db.execute( "select * from _arrows where _sourceuuid_=?" , [sourceuuid] ) do |row|
+            answer << row["_targetuuid_"]
+        end
+        db.close
+        answer
+    end
+
+    # Arrows::sourcesuuids(targetuuid)
+    def self.sourcesuuids(targetuuid)
+        db = SQLite3::Database.new(Arrows::databaseFilepath())
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        answer = []
+        db.execute( "select * from _arrows where _targetuuid_=?" , [targetuuid] ) do |row|
+            answer << row["_sourceuuid_"]
+        end
+        db.close
+        answer
+    end
+
+    # Arrows::childrenEntities(parentuuid)
+    def self.childrenEntities(parentuuid)
+        Arrows::targetsuuids(parentuuid)
+            .map{|uuid| NxEntities::getEntityByIdOrNull(uuid) }
+            .compact
+    end
+
+    # Arrows::parentsEntities(childuuid)
+    def self.parentsEntities(childuuid)
+        Arrows::sourcesuuids(childuuid)
+            .map{|uuid| NxEntities::getEntityByIdOrNull(uuid) }
+            .compact
+    end
+end
--- Libs/loader.rb
+++ Libs/loader.rb
@@ -61,6 +61,8 @@ require "/Users/pascal/Galaxy/LucilleOS/Libraries/Ruby-Libraries/KeyValueStore.r
 
 # ------------------------------------------------------------
 
+require_relative "Arrows.rb"
+
 require_relative "BinaryBlobsService.rb"
 
 require_relative "Config.rb"
