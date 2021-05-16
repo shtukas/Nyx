4848ebc3acad93caef64375cfc17afe57c0f2a5a
Pascal Honoré <pascal@alseyn.net>
2021-05-16 19:07:05 +0100

    Introduce NxEntity

--- Go-Implementation/nyx.go
+++ Go-Implementation/nyx.go
@@ -18,12 +18,18 @@ type Nx27 struct {
 	payload  string
 }
 
+type NxEntity struct {
+	uuid       string
+	entityType string
+	datetime   string
+}
+
 func main() {
 	fmt.Println("Hello World!")
 	for _, id := range space.SpaceIds() {
 		fmt.Println(space.SpaceId2MarbleFilepath(id))
 	}
 	fmt.Println("Pascal")
-	fmt.Println(Nx19{"announce", "6c249256-2379-4683-bc31-23bbcce4fd39"})
-	fmt.Println(Nx27{"e1cec0c2-f1ad-4411-9f59-d25cf6bdfa4b", "2021-05-16T17:41:45Z", "unique-string", "a301c45a-e0d1"})
+	fmt.Println("Nx19", Nx19{"announce", "6c249256-2379-4683-bc31-23bbcce4fd39"})
+	fmt.Println("Nx27", Nx27{"e1cec0c2-f1ad-4411-9f59-d25cf6bdfa4b", "2021-05-16T17:41:45Z", "unique-string", "a301c45a-e0d1"})
 }
