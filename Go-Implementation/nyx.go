222acfe67607fd702ea790dfb9a5731d6153a7c8
Pascal Honoré <pascal@alseyn.net>
2021-05-16 19:24:36 +0100

    Introduce NxEvent1

--- Go-Implementation/nyx.go
+++ Go-Implementation/nyx.go
@@ -17,7 +17,14 @@ type Nx27 struct {
 	type_    string
 	payload  string
 }
+type NxEvent1 struct {
+	uuid        string
+	datetime    string
+	date        string
+	description string
+}
 
+// NxEntity is representing a super type of Nx27 and NxEvent1
 type NxEntity struct {
 	uuid       string
 	entityType string
@@ -32,4 +39,6 @@ func main() {
 	fmt.Println("Pascal")
 	fmt.Println("Nx19", Nx19{"announce", "6c249256-2379-4683-bc31-23bbcce4fd39"})
 	fmt.Println("Nx27", Nx27{"e1cec0c2-f1ad-4411-9f59-d25cf6bdfa4b", "2021-05-16T17:41:45Z", "unique-string", "a301c45a-e0d1"})
+	fmt.Println("NxEvent1", NxEvent1{"6c249256-2379-4683-bc31-23bbcce4fd39", "2021-05-16T18:14:12Z", "2021-05-16", "description"})
+	fmt.Println("NxEntity", NxEntity{"6c249256-2379-4683-bc31-23bbcce4fd39", "Nx27", "2021-05-16T18:14:12Z"})
 }
