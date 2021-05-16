7748757424652c66c0192a942a5be1a148a514d1
Pascal Honoré <pascal@alseyn.net>
2021-05-16 19:54:54 +0100

    Introduce NxListing

--- Go-Implementation/nyx.go
+++ Go-Implementation/nyx.go
@@ -24,6 +24,12 @@ type NxEvent1 struct {
 	description string
 }
 
+type NxListing struct {
+	uuid        string
+	datetime    string
+	description string
+}
+
 // NxEntity is representing a super type of Nx27 and NxEvent1
 type NxEntity struct {
 	uuid       string
@@ -40,5 +46,6 @@ func main() {
 	fmt.Println("Nx19", Nx19{"announce", "6c249256-2379-4683-bc31-23bbcce4fd39"})
 	fmt.Println("Nx27", Nx27{"e1cec0c2-f1ad-4411-9f59-d25cf6bdfa4b", "2021-05-16T17:41:45Z", "unique-string", "a301c45a-e0d1"})
 	fmt.Println("NxEvent1", NxEvent1{"6c249256-2379-4683-bc31-23bbcce4fd39", "2021-05-16T18:14:12Z", "2021-05-16", "description"})
+	fmt.Println("NxListing", NxListing{"6c249256-2379-4683-bc31-23bbcce4fd39", "2021-05-16T18:14:12Z", "description"})
 	fmt.Println("NxEntity", NxEntity{"6c249256-2379-4683-bc31-23bbcce4fd39", "Nx27", "2021-05-16T18:14:12Z"})
 }
