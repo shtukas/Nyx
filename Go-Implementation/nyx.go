862f18833b772cb13cb54ea0d06d5dbc63b61798
Pascal Honoré <pascal@alseyn.net>
2021-05-16 18:33:50 +0100

    (Re)Introduce Nx19

--- Go-Implementation/nyx.go
+++ Go-Implementation/nyx.go
@@ -6,9 +6,16 @@ import (
 	"github.com/shtukas/nyx/space"
 )
 
+type Nx19 struct {
+	announce string
+	uuid     string
+}
+
 func main() {
 	fmt.Println("Hello World!")
 	for _, id := range space.SpaceIds() {
 		fmt.Println(space.SpaceId2MarbleFilepath(id))
 	}
+	fmt.Println("Pascal")
+	fmt.Println(Nx19{"announce", "6c249256-2379-4683-bc31-23bbcce4fd39"})
 }
